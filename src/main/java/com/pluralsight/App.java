752196eb36fd8fb38439948dc0fe7443c023b709
jihaddrake <ringod40@gmail.com>
2026-04-07 10:59:43 -0400

    Added addtional print statment

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -3,5 +3,6 @@ package com.pluralsight;
 public class App {
     public static void main(String[] args) {
         System.out.println("Testing to see if it works");
+        System.out.println("Fish pog Fish pog fish Pog");
     }
 }
