cd824c74603fa208955a8ce904cdcdea3db6f820
jihaddrake <ringod40@gmail.com>
2026-04-07 11:09:15 -0400

    Added 2 additional print statement ("Buzz Buzz Buzz")("Another one")

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -4,5 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Testing to see if it works");
         System.out.println("Fish pog Fish pog fish Pog");
+        System.out.println("Buzz Buzz Buzz");
+        System.out.println("Another one");
     }
 }
